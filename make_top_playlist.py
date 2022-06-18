2e3d7bef9045b81137464c6cfd9613d761ad6251
Harry Kim <harry4011@gmail.com>
2022-06-18 04:57:23 -0300

    os path error updated

--- make_top_playlist.py
+++ make_top_playlist.py
@@ -1,3 +1,5 @@
+#!/Users/harrykim/opt/anaconda3/bin/python
+
 import os
 import pandas as pd
 import json
@@ -6,8 +8,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import json
 
 #client data from spotify dev for this app 
-client_ID = 'enter_client_ids'
-client_Secret = 'enter_client_keys'
+client_ID = '63ab47dce765482480375e7708b13092'
+client_Secret = '0a68a8c15ee14d9baa842e1051f815b3'
 
 #using spotipy, authenticating
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -22,12 +24,15 @@ user_id = sp.current_user()['id']
 
 def create_playlist(user_id,term):
         playlist_name = 'My top songs ' + term
-        playlist = sp.user_playlist_create(user= user_id, name= playlist_name,collaborative=False)
+        detail = "Automatically updated top songs " + term + ". Script at https://github.com/harrykim7/Top-Spotify-Songs"
+        playlist = sp.user_playlist_create(user= user_id, name= playlist_name,collaborative=False,description = detail)
         return playlist['id']
 
 
 #we only need to create the playlist once. 
-repeat = os.path.isfile("playlist_ids.txt") 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.join(dir_path, "playlist_ids.txt")
+repeat = os.path.isfile(file_path) 
 
 if repeat == False:
     #creating the playlists for the first time 
@@ -35,23 +40,22 @@ if repeat == False:
     medium_id = create_playlist(user_id,'from the last 6 months')
     long_id = create_playlist(user_id,'all time')
 
-    print(short_id)
     #storing the playlist ids 
     playlist_ids = {'short_term':short_id,'medium_term':medium_id,'long_term':long_id}
-    f = open("playlist_ids.txt", "w")
+    f = open(file_path, "w")
     f.write(str(playlist_ids))
     f.close()
 
 
 
 #grabbing the playlist ids
-f = open("playlist_ids.txt", "r")
+f = open(file_path, "r")
 content= f.read().replace("'", '"')
 ids = json.loads(content)
 
 #check if the playlist has been deleted ------------should this exist? on one hand, if accidently deleted, this is useful. On the other, this forces a playlist until script is stopped
 still_here = sp.playlist_is_following(ids['short_term'],user_ids= [user_id])
-x = still_here[0]
+
 def update_playlist(user_id,period,playlist_id):
     results = sp.current_user_top_tracks(limit=50, offset=0, time_range=period)
     songs = pd.DataFrame(results['items'])
